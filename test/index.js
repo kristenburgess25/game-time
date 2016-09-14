07af622e577bb31e41ae82807dfa981b5a63d0ff
Devin Beliveau <devinmarieb@gmail.com>
2016-09-14 13:08:26 -0600

    Add sudo coding_db

---
+++ test/index.js
@@ -0,0 +1,35 @@
+
+//HTML
+//keyboard
+//reset game button
+//load new word button(to play again, not necessary on page load)
+//give up button (don't have to have, just an idea)
+//<p>s to generate text like 'you have x tries left', 'you got it! load new word'
+
+//CSS
+//Deal with this shit later
+//keyboard look
+//background image
+//hangman image size
+
+//JAVASCRIPT
+//array for possible words
+
+//function for random word generator (math.random)
+//function to generate new word on page load
+//function to add empty spaces to gameboard
+//function to check if letter matches word
+//function to put letter in word on gameboard
+//function to draw hangman (keep replacing one image with another, etc)
+//function to block keys/grey out keys that have already been hit
+//function(s) to display <p> texts upon certain actions
+//function to dispay give up message(if we choose to do this)
+//function to make reset game button generate new word and reset keyboard
+//function to make load new word button generate new word and reset keyboard
+
+//keyboard hot keys
+
+//UP LEVEL OF DIFFICULITY
+//words get harder/longer when you win more
+//OR
+//hangman dies faster upon multiple plays
