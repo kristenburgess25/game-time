73ea22177e16efdedb22c6832520ed1945ed19da
Devin Beliveau <devinmarieb@gmail.com>
2016-09-19 11:17:11 -0600

    WIP CheckLetter functionality

--- test/game-time-test.js
+++ test/game-time-test.js
@@ -4,11 +4,10 @@ const HangMan = require("../lib/game-time");
 
 
 describe ("Hang Man", function(){
-
   var hangMan = new HangMan();
 
   it('should be a function', function(){
-    assert.isFunction(hangMan.getLevTwoWord);
+    assert.isFunction(hangMan.getLevOneWord);
   });
 
   it('should return a random number integer between 0 and 5', function(){
@@ -16,20 +15,22 @@ describe ("Hang Man", function(){
   });
 
   it('should return an array with string values', function(){
-    assert.isString(hangMan.getLevTwoWord());
+    assert.isString(hangMan.getLevOneWord());
   });
 
-  // it('should return an expected string if given the correct index', function(){
-  //   var hangman = new Hangman();
-  //   hangman.getLevTwoWord();
-  //
-  // });
+  it('should instantiate a new Hangman game', function(){
+    assert.isObject(hangman);
+  });
+
+});
 
 
+describe ("Check Letter", function(){
+  var hangMan = new HangMan();
 
-  // it('should return a string within levTwo word Array', function() {
-  //   var 1 = Math.random()* levTwoWord.length);
-  //   assert.equal(hangMan.getLevTwoWord, 'cranberry');
-  // })
+  it('should be a function', function(){
+    assert.isFunction(hangMan.checkLetter);
+  });
+
 
 });
